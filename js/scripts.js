3b92069d0b094b3deb35b823599db465bbeb4c7f
Gracie Lundell <gracielundell@gmail.com>
2016-03-06 16:21:18 -0800

    get squares to turn the color of the circle clicked

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+var squareColor = "red";
+
 var circleArray = [
   {
     color: "darkkhaki"
@@ -14,24 +16,24 @@ function createCircleDiv(circle) {
   return "<div class='circle' style='background:" + circle.color + "'></div>"
 };
 
-
 $(document).ready(function() {
   circleArray.forEach(function(circle) {
     var circleDiv = createCircleDiv(circle);
     $(".circles").append(circleDiv);
   });
 
-
+// set orig square color
   $(".wrapper").click(function(event) {
     console.log(event.target);
+    $(event.target).css("background", squareColor);
   });
 
+
+// set square color to circle color
   $(".circle").click(function(event) {
     console.log(event.target);
+    squareColor = $(event.target).css("background");
+
   });
-
-
-
-
 
 });
